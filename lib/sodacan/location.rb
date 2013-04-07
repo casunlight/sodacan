5ec7b72c989675a8ee9889434a5aee8c2aaf94c3
Tyler Heck <tyler.heck@gmail.com>
2013-04-07 10:36:45 -0400

    removed location dependence on optional field: human_address

--- lib/sodacan/location.rb
+++ lib/sodacan/location.rb
@@ -3,10 +3,8 @@ module SodaCan
     def initialize (params)
       lat = Float(params['latitude'])
       long = Float(params['longitude'])
-      addr = JSON.parse(params['human_address'])
       define_singleton_method(:latitude){ lat }
       define_singleton_method(:longitude){ long }
-      define_singleton_method(:human_address){ addr }
     end
   end
 end
