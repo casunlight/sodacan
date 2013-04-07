11b16044f4272a234e6bc1880a517028108e5af3
Trevor John, tjohn <pair+trevor@john.tj>
2013-04-07 10:44:44 -0400

    Use self class syntax

--- lib/sodacan/base.rb
+++ lib/sodacan/base.rb
@@ -1,55 +1,6 @@
 module SodaCan
   class Base
 
-    @@url = nil
-
-    # CLASS Methods
-    
-    def self.all
-      self.send_query @@url
-    end
-
-    def self.connect (url)
-      @@url = url.to_s
-    end
-
-    def self.execute (soql)
-      query = "#{ @@url }?$query=#{ soql }"
-      self.send_query query
-    end
-
-    def self.find (id)
-      self.where(id: id).first
-    end
-
-    def self.search (term)
-      query = "#{ @@url }?$q=#{ term }"
-      self.send_query query
-    end
-
-    def self.where (params)
-      query = "#{ @@url }?"
-
-      if params.has_key?(:id)
-        query << "$limit=1&$offset=#{ params[:id].to_i-1 }" 
-        params.delete(:id)
-      end
-
-      unless params.empty?
-        query << "$where="
-
-        query = params.to_a.reduce(query) do |q,pair|
-          q << "#{ pair[0] }=\"#{ pair[1] }\"&"
-        end
-        query = query[0..-2]
-      end
-
-      self.send_query query
-    end
-
-
-    # INSTANCE METHODS
-
     def initialize (data, fields)
       data.each do |key,val|
         method_name = key.to_sym
@@ -74,19 +25,68 @@ module SodaCan
       end
     end
 
-    protected
 
-    def self.send_query (query)
-      response = RestClient.get query
+    class << self
 
-      field_names = JSON.parse(response.headers[:x_soda2_fields])
-      field_types = JSON.parse(response.headers[:x_soda2_types])
-      fields = field_names.zip(field_types).reduce({}) do |memo, pair|
-        memo[pair[0].to_sym] = pair[1]
-        memo
+      @@url = nil
+
+      def all
+        send_query @@url
       end
 
-      JSON.parse(response.body).map{ |data_hash| self.new(data_hash,fields) }
+      def connect (url)
+        @@url = url.to_s
+      end
+
+      def execute (soql)
+        query = "#{ @@url }?$query=#{ soql }"
+        send_query query
+      end
+
+      def find (id)
+        where(id: id).first
+      end
+
+      def search (term)
+        query = "#{ @@url }?$q=#{ term }"
+        send_query query
+      end
+
+      def where (params)
+        query = "#{ @@url }?"
+
+        if params.has_key?(:id)
+          query << "$limit=1&$offset=#{ params[:id].to_i-1 }"
+          params.delete(:id)
+        end
+
+        unless params.empty?
+          query << "$where="
+
+          query = params.to_a.reduce(query) do |q,pair|
+            q << "#{ pair[0] }=\"#{ pair[1] }\"&"
+          end
+          query = query[0..-2]
+        end
+
+        send_query query
+      end
+
+
+      protected
+
+      def send_query (query)
+        response = RestClient.get query
+
+        field_names = JSON.parse(response.headers[:x_soda2_fields])
+        field_types = JSON.parse(response.headers[:x_soda2_types])
+        fields = field_names.zip(field_types).reduce({}) do |memo, pair|
+          memo[pair[0].to_sym] = pair[1]
+          memo
+        end
+
+        JSON.parse(response.body).map{ |data_hash| self.new(data_hash,fields) }
+      end
     end
   end
 end
