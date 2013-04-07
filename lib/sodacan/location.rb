b272dced15bb44a12c9ae2740de9a82c97d6fb52
Tyler Heck <tyler.heck@gmail.com>
2013-04-07 11:22:27 -0400

    handling for poorly formatted locations

--- lib/sodacan/location.rb
+++ lib/sodacan/location.rb
@@ -1,8 +1,10 @@
 module SodaCan
   class Location
     def initialize (params)
-      lat = Float(params['latitude'])
-      long = Float(params['longitude'])
+      lat = nil
+      lat = Float(params['latitude']) if params['latitude']
+      long = nil
+      long = Float(params['longtitude']) if params['longitude']
       define_singleton_method(:latitude){ lat }
       define_singleton_method(:longitude){ long }
     end
